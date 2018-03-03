ff21bc0873a6559cb9bfd29974bb9c111206ce27
Stoyko <stoykostanchev@gmail.com>
2018-03-03 22:21:55 +0000

    Some commands and json processing

--- click_buttons.py
+++ click_buttons.py
@@ -4,18 +4,34 @@ import thread
 import time
 from Xlib import display
 from pynput import keyboard, mouse
+
 import numpy as np
 from mss import mss
 from PIL import Image
 import cv2
+import json
+
+data = json.load(open('data.json'))
+actions = []
+
+for action in data["actions"]:
+    keysRaw = [key.strip() for key in action["shortcut"].split("+")]
+    keys = set()
+
+    for key in keysRaw:
+        try:
+            keys.add(keyboard.Key[key])
+        except Exception:
+            keys.add(keyboard.KeyCode.from_char(key))
+    actions.append({ "name": action["name"], "keyVals": keys })
+
+
 
 
 sct = mss()
 
-# mon = {'top': 0, 'left': 0,'width': 0, 'height': 0 }
-
-COMBINATION = {keyboard.Key.shift, keyboard.Key.ctrl}
 current = set()
+
 
 def findBtnCoord(btnImgSrc):
     # screen = sct.grab(mon)
@@ -23,16 +39,16 @@ def findBtnCoord(btnImgSrc):
         # Get raw pixels from the screen
         sct_img = sct.grab(monitor)
         # Create the Image
-    
+
     screen = np.array(sct_img)
     screen = cv2.cvtColor(screen, cv2.COLOR_BGRA2GRAY)
     #cv2.imshow("Origin", screen)
-    #cv2.waitKey(0)    
-    #cv2.destroyAllWindows()     
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
     template = cv2.imread(btnImgSrc,0)
-    
+
     w, h = template.shape[::-1]
-    
+
     res = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
@@ -40,10 +56,10 @@ def findBtnCoord(btnImgSrc):
     #bottom_right = (top_left[0] + w, top_left[1] + h)
 
     #cv2.rectangle(screen ,top_left, bottom_right, 255, 2)
-    
+
     #cv2.imshow("Match", screen)
-    #cv2.waitKey(0)    
-    #cv2.destroyAllWindows()     
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
     return (top_left[0] + w/2, top_left[1] + h/2)
 
 
@@ -55,18 +71,20 @@ def clickBtn(btnImgSrc):
     pyautogui.moveTo(qp.root_x, qp.root_y)
 
 
-clickBtn("templates/wallMenu.png")
-clickBtn("templates/terminal.png")
-clickBtn("templates/x.png")
+#clickBtn("templates/wallMenu.png")
+#clickBtn("templates/terminal.png")
+#clickBtn("templates/x.png")
 
 
 
 def on_press(key):
-    if key in COMBINATION:
-        current.add(key)
+    current.add(key)
 
-    if all(k in current for k in COMBINATION) and pressed:
-        clickBtn('templates/wallMenu.png')
+    print(current)
+
+    for combo in actions:
+        if all(k in current for k in combo["keyVals"]):
+            print(combo["name"])
 
     if key == keyboard.Key.esc:
         return False
@@ -81,3 +99,5 @@ def on_release(key):
 def observe_keyboard():
    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
        listener.join()
+
+observe_keyboard()
